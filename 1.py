7f0686b6b5dcc222cf82633a2ce88134606f7b26
Adasiaa <babakapa0009@gmail.com>
2025-03-14 18:13:14 +0200

    1.py

--- 1.py
+++ 1.py
@@ -1,16 +1,17 @@
-# This is a sample Python script.
+def S(score):
+    if 0 <= score <= 49:
+        return "Незадовільно"
+    elif 50 <= score <= 69:
+        return "Задовільно"
+    elif 70 <= score <= 89:
+        return "Добре"
+    elif 90 <= score <= 100:
+        return "Відмінно"
+    else:
+        return "Некоректний бал"
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+try:
+    score = int(input("Введіть кількість балів (0-100): "))
+    print("Ваша оцінка:", S(score))
+except ValueError:
+    print("введіть число від 0 до 100")
